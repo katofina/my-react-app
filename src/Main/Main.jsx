e00424209e58966e496a67f703e71e01f9515580
katofina <askantya@tut.by>
2024-05-24 21:47:28 +0300

    fix: text, main

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -4,7 +4,7 @@ export default function Main() {
     return (
         <div className="main">
             <p className='pMain'>All the best for a whole lot less.</p>
-            <p className='pSecond'>Start your shoppinng from search.</p>
+            <p className='pSecond'>Start your shopping from search.</p>
         </div>
     )
 }
